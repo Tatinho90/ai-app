214c6b1dc6ef7c13ec1d726eab364cfd6fd91706
Tatinho90 <101295790+Tatinho90@users.noreply.github.com>
2023-07-12 16:10:37 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import ChatWindow from "./Components/Chat window"
 
 function App() {
   return (
-   <></>
+   <>
+   <ChatWindow/>
+   </>
   );
 }
 
---
+++ src/Components/Chat window.js
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+import image from "../images/wallpaper.png"
+
+const StyledDiv = styled.div`
+width: 100%;
+height: 100vh;
+max-width: 325px;
+max-height: 700px;
+background-color: white;
+margin: auto;
+position: absolute;
+top: 0; right: 0;
+bottom: 0; left: 0;
+border-radius: 20px;
+background: url(${image}) no-repeat;
+background-color: #353535;
+`
+
+export default function Chatwindow(){
+    return (
+        <StyledDiv/>
+    )
+}
