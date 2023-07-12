1ad0e081cfdf932edc8a8340593150f7734759c8
Tatinho90 <101295790+Tatinho90@users.noreply.github.com>
2023-07-12 18:37:28 +0200

    Update celebrities.js

---
+++ src/celebrities.js
@@ -0,0 +1,17 @@
+const celebrities = [
+   {
+    firstName: "William",
+    lastName: "Shakespeare",
+    profilePicURL: "",
+   },
+   {
+    firstName: "George",
+    lastName: "Washington",
+    profilePicURL: "",
+   },
+   {
+    firstName: "Albert",
+    lastName: "Einstein",
+    profilePicURL: "",
+   },
+]
